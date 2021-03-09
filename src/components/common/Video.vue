d908bb3d4ec6a5514b0f8cca44cf82e8c61e2b94
bshz <sibo_he@qq.com>
2021-03-09 21:25:43 +0800

    Add video.js and update video source to m3u8

--- src/components/common/Video.vue
+++ src/components/common/Video.vue
@@ -1,44 +1,66 @@
 <template>
   <div class="video-container">
     <div class="video-wrapper">
-      <img ref="img" :src="placeholder" alt="" class="video">
+<!--      <img ref="img" :src="placeholder" alt="" class="video">-->
+      <video-player
+          ref="video"
+          :options="playerOptions"
+          class="video"
+      >
+        <div class="switch-button">
+          <el-button
+              v-for="item in sources"
+              :key="item.name"
+              @click="switchSource(item.value)"
+          >
+            {{ item.name }}
+          </el-button>
+        </div>
+      </video-player>
     </div>
   </div>
 </template>
 
 <script>
 import { server } from "../../../config";
-import icbc from '@/assets/img/icbc.jpg'
-import qs from "qs";
+import icbc from '@/assets/img/icbc.jpg';
+import VideoPlayer from "@/components/common/VideoPlayer";
+import 'video.js/dist/video-js.css'
 
 export default {
   name: "Video",
+  components: { VideoPlayer },
   data() {
     return {
-      source: "http://211.67.20.171:8081/hls/test2.m3u8",
       playerOptions: {
         autoplay: true,
-        sources: [{
-          type: "application/x-mpegURL",
-          src: "http://211.67.20.171:8081/hls/test2.m3u8"
-        }],
-      },
-      player: null,
-      videoOptions: {
-        autoplay: true,
         controls: true,
-        sources: [{
-          src: "http://211.67.20.171:8081/hls/test2.m3u8",
-          type: "application/x-mpegURL"
-        }]
+        sources: [
+          {
+            withCredentials: false,
+            type: 'application/x-mpegURL',
+            src: 'http://211.67.21.65:8081/hls/test2.m3u8'
+          }
+        ]
       },
       placeholder: icbc,
+      sources: [
+        {
+          name: '人脸检测',
+          value: 'http://211.67.21.65:8081/hls/test2.m3u8'
+        },
+        {
+          name: '遗留物品检测',
+          value: 'https://logos-channel.scaleengine.net/logos-channel/live/biblescreen-ad-free/playlist.m3u8'
+        }
+      ],
       ws: null
     }
   },
   mounted() {
     this.openWebSocket();
-    this.$refs.img.addEventListener('click', this.handleClick);
+    console.log(this.$refs.video.$refs.videoPlayer)
+    // this.$refs.img.addEventListener('click', this.handleClick);
   },
   beforeDestroy() {
     this.ws && this.ws.close(1000, 'video destroy')
@@ -62,12 +84,16 @@ export default {
       this.axios.post(server().http.selectPerson, {x: perX, y: perY})
           .then(e => console.log(e))
           .catch(error => console.error(error.message))
+    },
+    switchSource(v) {
+      console.log(v);
+      this.$refs.video.player.src({src: v, type: 'application/x-mpegURL'})
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .video-container {
   background: #061123;
   border-radius: 5px;
@@ -76,25 +102,56 @@ export default {
   flex-direction: column;
   height: 60%;
   margin: 0.5rem;
-}
 
-.video-container .title {
-  margin: 5px;
-}
+  .video-wrapper {
+    width: 100%;
+    height: 0;
+    padding: 5px 5px;
+    box-sizing: border-box;
+    flex: 1;
+    display: flex;
+    justify-content: center;
 
-.video-wrapper {
-  /*height: 100%;*/
-  width: 100%;
-  height: 0;
-  padding: 5px 5px;
-  box-sizing: border-box;
-  flex: 1;
-  display: flex;
-  justify-content: center;
-}
+    .video {
+      width: 100%;
+      height: 100%;
+      position: relative;
 
-.video {
-  max-width: 100%;
-  height: auto;
+      /deep/ .video-js {
+        width: 100%;
+        height: 100%;
+      }
+
+      .switch-button {
+        position: absolute;
+        top: 50%;
+        right: 2rem;
+        transform: translate(50%, -50%);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        opacity: 0;
+        transition: opacity .4s ease, transform .4s ease;
+
+        button {
+          width: 100%;
+          margin: 0;
+          background: rgba(255, 255, 255, 0.7);
+
+          & + button {
+            margin-top: 10px;
+          }
+        }
+      }
+
+      &:hover {
+        .switch-button {
+          opacity: 1;
+          transform: translate(0, -50%);
+        }
+      }
+    }
+  }
 }
 </style>
